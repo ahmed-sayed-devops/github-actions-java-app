9c3b825b5372d9bf663071ca1bfdca715241a2cd
Ahmed Sayed <ahmed.s88.devops@gmail.com>
2026-03-26 11:05:27 +0200

    add all project files

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,24 @@
+package com.mycompany.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AppTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHomeEndpoint() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+}
